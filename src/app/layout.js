8b58b382469d50d86b81bb30d941f54d6102f020
Arif243 <111947842+Arif243@users.noreply.github.com>
2025-08-03 14:06:39 +0530

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Happy Friendship Day SAlMA!",
+  description:
+    "A heartfelt surprise filled with memories, emotions, and a fun little game made just for you.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`bg-black`}>{children}</body>
+    </html>
+  );
+}
